95eb65fab8b5ebf324fdd1a834e4a785c302cc00
Julian Stieß <j_stiess17@cs.uni-kl.de>
2021-08-12 16:56:26 +0200

    delicious sauce mmhh

---
+++ src/scripts/utility.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    // Bootstrap animated dropdown
+    $('.navbar .dmenu').hover(function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
+        }, function () {
+            $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
+        });
+});
