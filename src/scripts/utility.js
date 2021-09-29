bfdd5125577e0649a9ee2d1db061126cb6a15bb2
Julian Stieß <j_stiess17@cs.uni-kl.de>
2021-09-29 18:15:09 +0200

    uwu :eggplant:

--- src/scripts/utility.js
+++ src/scripts/utility.js
@@ -1,8 +1,19 @@
+const spypad = 400
 $(document).ready(function () {
     // Bootstrap animated dropdown
+    $("#scrollspy").css("top", Math.max(0, spypad));
     $('.navbar .dmenu').hover(function () {
             $(this).find('.sm-menu').first().stop(true, true).slideDown(150);
         }, function () {
             $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
         });
+
+    // Keep Scrollspy fixed
+    $(window).scroll(function(){
+        $("#scrollspy").css("top", Math.max(0, spypad - $(this).scrollTop()));
+    });
+
+    $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
+        console.log("Mmhh Monke")
+      })
 });
