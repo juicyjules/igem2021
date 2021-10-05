143e883868a0f5e949846bfd7de06b56cade1852
Julian Stieß <j_stiess17@cs.uni-kl.de>
2021-10-05 14:03:18 +0200

    geilo :eggplant:

--- src/scripts/utility.js
+++ src/scripts/utility.js
@@ -1,5 +1,15 @@
-const spypad = 400
+function vh(v) {
+    var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    return (v * h) / 100;
+}
+function vw(v) {
+    var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (v * w) / 100;
+}
+const fadeScrollLimit = vh(100)
 $(document).ready(function () {
+    const spypad = $("#header").outerHeight() + $("#navigation").outerHeight() + 30;
+
     // Bootstrap animated dropdown
     $("#scrollspy").css("top", Math.max(0, spypad));
     $('.navbar .dmenu').hover(function () {
@@ -7,13 +17,22 @@ $(document).ready(function () {
         }, function () {
             $(this).find('.sm-menu').first().stop(true, true).slideUp(105)
         });
-
+    $("#go-up").on("click" ,function(){
+        window.scrollTo(0, 0)
+    });
     // Keep Scrollspy fixed
     $(window).scroll(function(){
         $("#scrollspy").css("top", Math.max(0, spypad - $(this).scrollTop()));
+        if (window.scrollY > fadeScrollLimit){
+            $("#go-up").css("opacity",1);
+        } else {
+            $("#go-up").css("opacity",0);
+        }
     });
 
     $('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
         console.log("Mmhh Monke")
       })
+
+    
 });
